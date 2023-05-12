c10931d74a610f2a63c089606a9d4db77edc3d48
kawarimidoll <kawarimidoll+git@gmail.com>
2023-05-12 20:50:29 +0900

    style: fix format

--- scripts/content_scripts.js
+++ scripts/content_scripts.js
@@ -10,28 +10,28 @@ function main(e) {
   function jsLoaded() {
     const selector = 'a.css-1qaijid.r-1loqt21[role="link"]';
     [...document.querySelectorAll(selector)]
-      .filter((e) => e.classList.length === 2).forEach(
-        (e) => {
-          if (e.innerText.endsWith("...") || isExternal(e.href)) {
-            const href = e.href.replace(/^https?:\/\//, "");
-            const innerText = e.innerText.replace("...", "");
-            const dst = decodeURI(e.href);
-            if (href.startsWith(innerText)) {
-              // overwrite
-              e.innerText = dst;
-            } else {
-              // beside
-              const span = document.createElement("span");
-              span.innerText = ` [💙😎 ${dst} ]`;
-              const parentFontSize = e.style["font-size"]?.replace("px", "");
-              const fontSize = Number(parentFontSize || "16") - 4;
-              span.style["font-size"] = `${fontSize}px`;
+      .filter((e) => e.classList.length === 2)
+      .forEach((e) => {
+        if (e.innerText.endsWith("...") || isExternal(e.href)) {
+          const href = e.href.replace(/^https?:\/\//, "");
+          const innerText = e.innerText.replace("...", "");
+          const dst = decodeURI(e.href);
 
-              e.appendChild(span);
-            }
+          if (href.startsWith(innerText)) {
+            // overwrite
+            e.innerText = dst;
+          } else {
+            // beside
+            const span = document.createElement("span");
+            span.innerText = ` [💙😎 ${dst} ]`;
+            const parentFontSize = e.style["font-size"]?.replace("px", "");
+            const fontSize = Number(parentFontSize || "16") - 4;
+            span.style["font-size"] = `${fontSize}px`;
+
+            e.appendChild(span);
           }
-          e.classList.add("bluespy-skip");
-        },
-      );
+        }
+        e.classList.add("bluespy-skip");
+      });
   }
 }
